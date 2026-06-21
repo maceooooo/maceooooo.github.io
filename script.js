a43f47821d7c1817d4119ab74784707cb5906bf0
garniem <Maceo.Garnier@etu.univ-grenoble-alpes.fr>
2026-06-21 19:40:47 +0200

    21/06/2026 update

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function copierTexte(element) {
-    const texte = element.innerText;
-    navigator.clipboard.writeText(texte).then(() => {
-      alert("Texte copié !");
-    }).catch(err => {
-      console.error("Erreur lors de la copie :", err);
+  const texte = element.innerText;
+  navigator.clipboard.writeText(texte).then(() => {
+    alert("Adresse Mail copiée !");
+  }).catch(err => {
+    console.error("Erreur lors de la copie :", err);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("voirPlusBtn");
+    const section = document.getElementById("projets");
+
+    btn.addEventListener("click", () => {
+        section.classList.toggle("open");
+
+        btn.textContent = section.classList.contains("open")
+            ? "Voir moins"
+            : "Voir plus";
     });
-  }
+});
